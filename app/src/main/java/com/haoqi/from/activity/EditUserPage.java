6fba6a8a3d69cbaa9b9a7184578e8aa2e1ce5d59
youxifuhuaqi <zah5897@gmail>
2016-01-06 10:36:34 +0800

    ss

--- app/src/main/java/com/haoqi/from/activity/EditUserPage.java
+++ app/src/main/java/com/haoqi/from/activity/EditUserPage.java
@@ -1,18 +1,79 @@
 package com.haoqi.from.activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.haoqi.from.R;
 import com.haoqi.from.base.BaseActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by youxifuhuaqi on 2015/12/29.
  */
 public class EditUserPage extends BaseActivity {
+    @Bind(R.id.back)
+    ImageView back;
+    @Bind(R.id.title)
+    TextView title;
+    @Bind(R.id.regist)
+    TextView regist;
+    @Bind(R.id.user_img)
+    ImageView userImg;
+    @Bind(R.id.user_img_layout)
+    RelativeLayout userImgLayout;
+    @Bind(R.id.name_txt)
+    TextView nameTxt;
+    @Bind(R.id.nick_name)
+    EditText nickName;
+    @Bind(R.id.man_box)
+    CheckBox manBox;
+    @Bind(R.id.woman_box)
+    CheckBox womanBox;
+    @Bind(R.id.most_like_text)
+    EditText mostLikeText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_edit);
+        ButterKnife.bind(this);
+    }
 
+    private ProgressDialog progressDialog;
+
+    @OnClick({R.id.btn_commit, R.id.back, R.id.user_img})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back:
+                onBackPressed();
+                break;
+            case R.id.user_img:
+                int selectedMode = MODE_SINGLE;
+                boolean showCamera = true;
+                int maxNum = 1;
+                Intent intent = new Intent(getActivity(), MultiImageSelectorActivity.class);
+                // 是否显示拍摄图片
+                intent.putExtra(MultiImageSelectorActivity.EXTRA_SHOW_CAMERA, showCamera);
+                // 最大可选择图片数量
+                intent.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_COUNT, maxNum);
+                // 选择模式
+                intent.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_MODE, selectedMode);
+                // 默认选择
+                startActivityForResult(intent, REQUEST_IMAGE);
+                break;
+            case R.id.btn_commit:
+
+                break;
+        }
     }
 }
--- app/src/main/java/com/haoqi/from/activity/LoginActivity.java
+++ app/src/main/java/com/haoqi/from/activity/LoginActivity.java
@@ -1,14 +1,24 @@
 package com.haoqi.from.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.haoqi.from.R;
+import com.haoqi.from.app.UserManager;
+import com.haoqi.from.app.http.DefaultJsonHttpResponseHandler;
+import com.haoqi.from.app.http.HttpUtils;
+import com.haoqi.from.app.http.Urls;
+import com.haoqi.from.app.listener.CallBackListener;
 import com.haoqi.from.base.BaseActivity;
+import com.haoqi.from.util.ProgressDialogUtil;
+import com.haoqi.from.util.ToastUtil;
 import com.haoqi.from.view.EmailAutoCompleteTextView;
+import com.loopj.android.http.RequestParams;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -52,10 +62,38 @@ public class LoginActivity extends BaseActivity {
         finish();
     }
 
+    private ProgressDialog dialog;
+
     @OnClick(R.id.tv_login)
     public void login() {
         String name = etEmail.getText().toString();
         String password = etPassword.getText().toString();
+
+        if (TextUtils.isEmpty(name)) {
+            ToastUtil.show("账号不能为空!");
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            ToastUtil.show("密码不能为空!");
+            return;
+        }
+        dialog = ProgressDialogUtil.show(this, "正在登录...");
+        UserManager.getInstance().login(name, password, new CallBackListener() {
+            @Override
+            public void onSuccess(int statusCode, Object... obj) {
+
+                finish();
+                ProgressDialogUtil.dismiss(dialog);
+            }
+
+            @Override
+            public void onFailure(int statusCode, String message) {
+                ProgressDialogUtil.dismiss(dialog);
+                if (statusCode > 0) {
+                    ToastUtil.show("登录失败！" + message);
+                }
+            }
+        });
     }
 
 }
